f9617864f785e8f59fa241f7e2726164063957db
Tamhat <87254817+Tamhat@users.noreply.github.com>
2021-08-18 13:34:59 +0600

    js answeres

---
+++ assainment.js
@@ -0,0 +1,75 @@
+// ১. Variable কি ?
+
+ভেরিয়েবল হলো ডাটা সংরক্ষণের  যায়গা।
+যাতে যেকোনো কিছু রাখা যায় যেকনো নাম দেওয়া যায়।
+
+// ২. Variable কিভাবে লিখতে হয় ?
+example:
+var same = "efad";
+var same = 30;
+console.log(same);
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+variable এর value তে double quatation এর ভেতর যাই লেখি না কেন সেটাই string type variable.
+ex:
+var same = "efad";
+console.log(same);
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+
+variable এর value তে যদি double quatation না লেখে ক্যাবল কোন number দেই তবে সেটা number type variable .
+ex:
+var same = 30;
+console.log(same);
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+
+Boolan এমন একটি variable যাতে সম্ভাব্য তথ্য TRUE OR FALSE ছাড়া আর কোন value ব্যাবহার করা যায়না;
+ex:
+var boolan = false;
+console.log(boolan)
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+ex:
+
+// toLowerCase
+var rochona = "loreM sdjcmsdklcm kmcdsmm m mckm mxc m ddxmcksdmc;lsasmck ksdmclkcmsdmlsmcks skcmip ksvjsjn jsdncjksc jdscn jdnjdjs sjdjs sjdns jsnds jncsd sum";
+console.log(rochona.toLowerCase());
+
+// toUpperCase
+var rochona = "loreM sdjcmsdklcm kmcdsmm m mckm mxc m ddxmcksdmc;lsasmck ksdmclkcmsdmlsmcks skcmip ksvjsjn jsdncjksc jdscn jdnjdjs sjdjs sjdns jsnds jncsd sum";
+console.log(rochona.toUpperCase());
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+JavaScript এর তাদের মধ্যে গুরুত্ব পূর্ণ কিছু অপারেটর গুল হল +,-,*,/;
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+var sum =60;
+var alone= Math.abs(sum);
+console.log(alone);
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+var sum =60;
+var alone= Math.celi(sum);
+console.log(alone);
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+
+var sum =60;
+var alone= Math.floor(sum);
+console.log(alone);
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+var sum =60;
+var alone= Math.round(sum);
+console.log(alone);
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+var sum =60;
+var alone= Math.random(sum);
+console.log(alone);
+
+// asignment.js - GitHub Link
